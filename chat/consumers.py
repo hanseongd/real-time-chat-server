637466b5dc81cfc0e0bbd944d0c4fbaf92019d45
cliffechoi <chs5183@naver.com>
2019-04-19 14:56:49 +0900

    채팅방 자동 DB 생성, message 저장 추가

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,16 +1,18 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+from chat.models import ChatRoom, Message
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         # 문자, 숫자, 하이픈 및 마침표만 포함할 수 있음
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.current_room, self.chat_room_created = ChatRoom.objects.get_or_create(name=self.room_name)
 
         # Join room group
         await self.channel_layer.group_add(
-            self.room_group_name,
+            self.room_name,
             self.channel_name
         )
 
@@ -19,18 +21,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
-            self.room_group_name,
+            self.room_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
+    async def receive(self, **kwargs):
+        text_data_json = json.loads(kwargs['text_data'])
         message = text_data_json['message']
+        Message.objects.create(room=self.current_room, message=message)
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name,
+            self.room_name,
             {
                 'type': 'chat_message',
                 'message': message
@@ -38,8 +41,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from room group
-    async def chat_message(self, event):
+    async def chat_message(self, event):  # event => {'type': 'chat_message', 'message': message}
         message = event['message']
+
         await self.send(text_data=json.dumps({
             'message': message
         }))
--- chat/views.py
+++ chat/views.py
@@ -5,10 +5,19 @@ from django.shortcuts import render
 # Create your views here.
 from django.utils.safestring import mark_safe
 
+from chat.models import Message
+
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {'room_name_json': mark_safe(json.dumps(room_name))})
+    messages = list(Message.objects.filter(room__name=room_name).extra(
+        select={'created_at': 'DATETIME(created_at)'}
+    ).values('message', 'created_at'))
+
+    return render(request, 'chat/room.html', {
+        'room_name': json.dumps(room_name),
+        'messages': messages
+    })

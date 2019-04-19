497420e7ef8e71b21e2f653f8853814e40e638e3
cliffechoi <chs5183@naver.com>
2019-04-19 14:45:26 +0900

    Fix chat models
    1. message 생성 시간 추가
    2. message ordering 추가

--- chat/models.py
+++ chat/models.py
@@ -5,8 +5,18 @@ from django.db import models
 
 class ChatRoom(models.Model):
     name = models.TextField()
-    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return '%s' % self.name
 
 
 class Message(models.Model):
     room = models.ForeignKey(ChatRoom, models.CASCADE, related_name='message')
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return '%s(%s)' % (self.message, self.room.name)
